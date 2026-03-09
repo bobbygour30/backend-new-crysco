b8ecdbe3a813ca5cd58d26e2b0b2e2cf70c529d2
Rajat <rajat804@gmail.com>
2026-03-09 14:26:30 +0530

    nimbus js file update

--- utils/nimbus.js
+++ utils/nimbus.js
@@ -1,13 +1,24 @@
 import axios from "axios";
 
 export const getNimbusToken = async () => {
-  const res = await axios.post(
-    "https://ship.nimbuspost.com/api/v1/auth/login",
-    {
-      email: process.env.NIMBUS_EMAIL,
-      password: process.env.NIMBUS_PASSWORD,
-    }
-  );
+  try {
 
-  return res.data.data;
+    const res = await axios.post(
+      "https://ship.nimbuspost.com/api/v1/auth/login",
+      {
+        email: process.env.NIMBUS_EMAIL,
+        password: process.env.NIMBUS_PASSWORD,
+      }
+    );
+
+    console.log("Nimbus Login Response:", res.data);
+
+    return res.data.data.token;
+
+  } catch (error) {
+
+    console.log("Nimbus Login Error:", error.response?.data || error.message);
+    throw error;
+
+  }
 };
