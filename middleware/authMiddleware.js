43eca0092b75ea54ddeb5758c6c230754d3c4fa5
bobbygour30 <bobby30gour@gmail.com>
2026-03-26 11:49:43 +0530

    updated

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 
 export const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1]; // Bearer TOKEN
+    const token = req.headers.authorization?.split(" ")[1];
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
